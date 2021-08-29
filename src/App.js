da203ad98e95d14f0c8685929f3ac5cfff650515
Jackie Lewis <89713670+lewisjn@users.noreply.github.com>
2021-08-28 18:31:04 -0700

    Deleting some sample text

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ function App() {
         <p>
           Jackie is learning React!
         </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
